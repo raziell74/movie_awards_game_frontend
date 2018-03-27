6b72da4bcfaa84e5d225d0899c29d114b4a52a3c
Jordan Richmeier <jordan.richmeier2@gmail.com>
2018-03-27 08:17:39 -0600

    Add enzyme shallow rendering to test suite

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
+import {shallow} from 'enzyme';
+import {shallowToJson} from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('test app', () => {
+    test('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    test('Enzyme element detection', () => {
+        const output = shallow(
+            <App />
+        );
+        const title = output.find('h1.App-title');
+
+        expect(shallowToJson(output)).toMatchSnapshot();
+        expect(title.text()).toEqual('Welcome to React');
+    });
 });
